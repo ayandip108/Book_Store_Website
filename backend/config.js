1cf71f811ec4378b1cc92ddc87f0231a8fad7061
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2025-01-02 14:01:21 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = process.env.PORT 5555;
+export const PORT = process.env.PORT || 5555;
 export const mongoDBURL =
 'mongodb+srv://root:root@books-store-mern.v5viq.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
